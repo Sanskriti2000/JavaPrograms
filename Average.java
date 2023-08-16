c255f851d15dc9538f4977ea826548ae9666564e
sanskriti <singhsanskriti340@gmail.com>
2023-08-16 16:59:23 +0530

    Add files via upload

---
+++ Average.java
@@ -0,0 +1,17 @@
+package javaprogram;
+
+public class Average 
+{
+	public static void main(String args[])
+	{
+		int p=56; int c=65; int b =85;
+		int s;
+		float avg;
+		
+		s=(p+c+b);
+		avg=s/3;
+		System.out.println("The total sum of the marks="+s);
+		System.out.println("The average of the total marks obtained="+avg);
+	}
+
+}
