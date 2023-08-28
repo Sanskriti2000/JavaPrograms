4df6c94ed508812f2c82fdf106a48d3c997db200
sanskriti <singhsanskriti340@gmail.com>
2023-08-28 12:32:52 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,11 @@
+public class num {
+    public static void main(String args[]){
+        int i;i=0;
+        while(i<10){
+            i++;
+            if(i%2==0)
+                continue;
+            System.out.println(i);
+        }
+    }
+}
