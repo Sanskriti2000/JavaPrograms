8cbfb09bb203465489589ba6b738393c49cee8c4
sanskriti <singhsanskriti340@gmail.com>
2023-09-07 19:49:37 +0530

    Add files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,23 @@
+public class Fibonacci {
+    public static void main(String args[]) {
+        int a, b, c, n;
+        a = 0;
+        b = 1;
+        c = 0;
+        n = 3;
+
+        System.out.println("The Fibonacci Series is: ");
+        System.out.println(a);
+        System.out.println(b);
+        do
+            {
+                c = a + b;
+                System.out.println(c);
+                a = b;
+                b = c;
+                n = n + 1;
+            }
+        while (n <= 10) ;
+        }
+    }
+
