83514a9a7bb254df44789c737fcd32581a7e02d6
sanskriti <singhsanskriti340@gmail.com>
2023-09-09 19:39:08 +0530

    Add files via upload

---
+++ Series.java
@@ -0,0 +1,9 @@
+public class Series {
+    public static void main(String args[]){
+        int a;
+        float s; s=0;
+        for(a=1; a<=10; a++)
+        s=s+1/a;
+        System.out.println("The sum of the series ="+s);
+    }
+}
